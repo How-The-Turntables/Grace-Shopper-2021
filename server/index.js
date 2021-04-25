d63bdaf5b7b1858b078d2dbfc1fbad18244612cc
Kevin Gil <93kevingil@gmail.com>
2021-04-25 14:14:27 -0400

    added modules nodemon and express -- created init function running port

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const init = async () => {
+  try {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on port: ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
+
+module.exports = app;
