4110b180f1166f1f573193b0fb67f9fa1c2ac743
Kevin Gil <93kevingil@gmail.com>
2021-04-25 14:24:42 -0400

    removed async from init function

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const init = async () => {
+const init = () => {
   try {
     const port = process.env.PORT || 3000;
     app.listen(port, () => console.log(`listening on port: ${port}`));
