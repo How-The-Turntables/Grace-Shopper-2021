3fbc481e60fb73efa7b00d941cccde4133159172
Emily <emilypearlasaro@gmail.com>
2021-04-25 16:18:03 -0400

    started sync and seed in /server/db/seed.js

---
+++ server/db/seed.js
@@ -0,0 +1,13 @@
+const db = require('./db');
+const OrderDetail = require('./models/shopping');
+
+const syncAndSeed = async () => {
+  try {
+    await db.sync({ force: true });
+  }
+    catch (error) {
+      console.log('error seeding database!', error);
+    }
+  };
+
+  module.exports = syncAndSeed;
