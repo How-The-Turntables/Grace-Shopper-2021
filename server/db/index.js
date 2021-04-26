f724586cfd866f18bf921f622269b3d225ca41e2
Jonathan Crider <13684007+JonathanCrider@users.noreply.github.com>
2021-04-25 21:58:38 -0400

    created artist model

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,7 @@
 const OrderDetail = require('./models/shopping');
 const User = require('./models/users/user');
+const Album = require('./models/products/album');
+const Artist = require('.models/products/album');
 
 
 // OrderDetail.belongsTo( Product );
