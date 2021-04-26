a2699330ee286beaf2713250968792085244de83
Maciej Pie <maciejpiech18@gmail.com>
2021-04-25 22:01:24 -0500

    first commit

--- test/products.spec.js
+++ test/products.spec.js
@@ -1,7 +1,14 @@
 const { expect } = require('chai');
+const Album = require('../server/db/models/products/album');
 
 describe('Mocha/Chai Test', function () {
   it('should show a passing test', function () {
     expect(1 + 1).to.equal(2);
+  });
+});
+
+describe('Album', () => {
+  describe('can create a new album', () => {
+    it('it has a name', () => {});
   });
 });
