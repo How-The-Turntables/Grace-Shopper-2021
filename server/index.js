a27dd2bbba69e6b92821cac4f19f1f532060482c
Jonathan Crider <13684007+JonathanCrider@users.noreply.github.com>
2021-04-25 23:34:03 -0400

    Routes: added static route to root, added morgan, added error handling

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = require('./api/router');
+const path = require('path');
+const morgan = require('morgan');
 const syncAndSeed = require('./db/seed');
 
 const app = express();
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
 
 app.use('/api', router); // path to router for the back-end links
+
+app.get('/', )
 
 const init = async () => {
   try {
