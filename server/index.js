701d95649c7c410619901b290c184d94c3090759
Jonathan Crider <13684007+JonathanCrider@users.noreply.github.com>
2021-04-25 23:37:12 -0400

    Routes: added static route to root, added morgan, added error handling

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/public/inde
 
 app.use('/api', router); // path to router for the back-end links
 
-app.get('/', )
+app.use(function (req, res, next) {
+  res.status(404).send("Are you lost? That page doesn't seem to exist.")
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ error: err });
+});
 
 const init = async () => {
   try {
