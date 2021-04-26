47c0ee1b00bf6b9563469b29952aeb4a871d6a1b
Jonathan Crider <13684007+JonathanCrider@users.noreply.github.com>
2021-04-25 20:56:36 -0400

    Data Layer: changed products.js to album.js, completed model, added default image to image folder

---
+++ server/db/models/products/album.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const db = require('../../db');
+
+class Album extends Model {}
+
+Album.init({
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: DataTypes.TEXT
+  },
+  genre: {
+    type: DataTypes.ENUM(['HEAVY METAL','LIGHT JAZZ', 'GLAM ROCK', 'GOSPEL', 'ELEVATOR', 'DISCO', 'HIP-HOP', 'HAIR BANDS', 'MATH ROCK']),
+    allowNull: false
+  },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1700,
+      max: 2021,
+      notEmpty: true
+    }
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  photoUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isUrl: true
+    },
+    defaultValue: '/public/img/defaultAlbum.png'
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+},{
+  sequelize: db,
+  modelName: 'album'
+});
+
+module.exports = Album
