48b46dca402736877bb5a178196757819323d2a6
Maciej Pie <maciejpiech18@gmail.com>
2021-04-26 20:58:44 -0500

    need to switch to a different branch

--- server/api/router.js
+++ server/api/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const albumsRouter = require('./products');
 const ordersRouter = require('./orders');
+const artistRouter = require('./artists');
 
 router.use('/albums', albumsRouter);
 router.use('/orders', ordersRouter);
+router.use('/artists', artistRouter);
 
 module.exports = router;
