88eea5d7e496c505185395b493316a2722361a62
Kevin Gil <93kevingil@gmail.com>
2021-04-26 22:08:07 -0400

    user router added

--- server/api/router.js
+++ server/api/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const albumsRouter = require('./products');
 const ordersRouter = require('./orders');
+const usersRouter = require('./users');
 
 router.use('/albums', albumsRouter);
 router.use('/orders', ordersRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
