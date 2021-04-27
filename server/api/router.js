47b031f95b5ed660d712d18ec31bbea400db845d
Maciej Pie <maciejpiech18@gmail.com>
2021-04-27 17:15:17 -0500

    minor fix

--- server/api/router.js
+++ server/api/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const albumsRouter = require('./products');
+const albumsRouter = require('./albums');
 const ordersRouter = require('./orders');
 const artistRouter = require('./artists');
 
