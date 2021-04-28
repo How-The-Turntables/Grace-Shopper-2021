43c3dd90ff2707ff147500796c74085a1015cc03
Maciej Pie <maciejpiech18@gmail.com>
2021-04-28 15:34:35 -0500

    base created

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 // import { connect } from 'react-redux'
+
+//add styled code here to test if it works. If all goes well then ill move it to its separate file
+
+const Title = styled.h1`
+  font-size: 3.5em;
+  text-align: center;
+  font-weight: bold;
+  color: green;
+  margin-top: 0;
+`;
+
+// use as a container to hold divs inside
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// use as boxes inside of the Container
+const Box = styled.div`
+  width: 40%;
+  height: 20rem;
+  border: solid 1px black;
+  border-radius: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 3rem;
+
+  :hover {
+    width: 60%;
+    border: solid 4px green;
+    font-weight: bold;
+    color: black;
+    background-color: greenyellow;
+  }
+`;
 
 const Home = () => {
   return (
     <div>
-      <h2>How The Turntables | Bootleg Records For Your Ear Holes</h2>
+      <Title>How The Turntables | Bootleg Records For Your Ear Holes</Title>
+      <Container>
+        <Box>Browse by Albums</Box>
+        <Box>Browse by Artists</Box>
+      </Container>
     </div>
   );
 };
--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Home from './components/Home';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>SHIT IS WORKING</h1>
+        <Home />
       </div>
     );
   }
