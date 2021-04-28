8b6aab4a008a78f7dc9c571e551eeefbb4fa746c
Kevin Gil <93kevingil@gmail.com>
2021-04-28 18:40:52 -0400

    moved App component to App.js

--- client/index.js
+++ client/index.js
@@ -1,14 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>SHIT IS WORKING</h1>
-      </div>
-    );
-  }
-}
+import App from './components/App';
+
 
 ReactDOM.render(<App />, document.querySelector('#app'));
