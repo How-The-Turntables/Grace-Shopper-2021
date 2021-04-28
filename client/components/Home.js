1c0b606dc8d0046c4d74b12ed0a9e8508060d8b2
Maciej Pie <maciejpiech18@gmail.com>
2021-04-28 12:43:40 -0500

    basic code

---
+++ client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { connect } from 'react-redux'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>How The Turntables | Bootleg Records For Your Ear Holes</h2>
+    </div>
+  );
+};
+
+export default Home;
