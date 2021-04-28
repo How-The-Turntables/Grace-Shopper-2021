23f666acb9a676adaaac5b3bb96e0fb5a6a3ef2c
Kevin Gil <93kevingil@gmail.com>
2021-04-27 23:26:26 -0400

    frontend changes

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, './client/index.js'),
+  output: {
+    path: path.resolve(__dirname, './server/public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
