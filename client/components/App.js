e416d2af4b6796f746833cf9bcca5062bfbd03ba
Kevin Gil <93kevingil@gmail.com>
2021-04-28 22:55:54 -0400

    added Routes in App.js

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import { Nav, Home } from './index';
+import { Nav, Home, AllAlbums, AllArtists } from './index';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route component={Nav} />
-        <Route component={Home} />
+        <Route component={Home} path='/' exact/>
+        <Route component={AllAlbums} path='/albums'/>
+        <Route component={AllArtists} path='/artists'/>
       </div>
     );
   }
