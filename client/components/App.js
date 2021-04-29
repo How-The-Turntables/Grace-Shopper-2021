72e50d68ffca04dc5ffe66ded2d21c9f287f9924
Kevin Gil <93kevingil@gmail.com>
2021-04-28 21:14:32 -0400

    importing and exporting App & Nav components out of index.js

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Route, Link} from 'react-router-dom';
-import Nav from './Nav';
+import {Nav} from './index';
 
 class App extends Component {
   render() {
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router} from 'react-router-dom';
-import App from './components/App';
+import {App} from './components/index';
 
 
 ReactDOM.render(<Router><App /></Router>, document.querySelector('#app'));
