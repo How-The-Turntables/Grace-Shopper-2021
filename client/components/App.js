58c2455c5cacd1ef9364e2b4c835789667487eae
Jonathan Crider <13684007+JonathanCrider@users.noreply.github.com>
2021-04-29 23:24:36 -0400

    imported link to form view

--- client/components/App.js
+++ client/components/App.js
@@ -16,15 +16,15 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Route component={Nav} />
-        <Route component={Home} path="/" exact />
-        <Route component={LoginView} path="/login" />
-        <Route component={SignUpForm} path="/register" />
-        <Route component={AllAlbums} path="/albums" />
-        <Route component={AllArtists} path="/artists" />
-        <Route component={CartView} path="/cart" />
-        <Route component={Checkout} path="/checkout" />
-        <Route component={Footer} />
+        <Route component={ Nav } />
+        <Route component={ Home } path="/" exact />
+        <Route component={ LoginView } path="/login" />
+        <Route component={ SignUpForm } path="/register" />
+        <Route component={ AllAlbums } path="/albums" />
+        <Route component={ AllArtists } path="/artists" />
+        <Route component={ CartView } path="/cart" />
+        <Route component={ Checkout } path="/checkout" />
+        <Route component={ Footer } />
       </div>
     );
   }
--- client/components/LoginView.js
+++ client/components/LoginView.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import LoginForm from './LoginForm';
+import { Link } from 'react-router-dom';
+
 
 const LoginView = () => {
     return (
