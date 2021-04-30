52e72c871db3d8b727d36dc0688a9aacb3bc4679
Kevin Gil <93kevingil@gmail.com>
2021-04-29 21:38:04 -0400

    added CartView component

--- client/components/index.js
+++ client/components/index.js
@@ -3,5 +3,6 @@ import Nav from './Nav';
 import Home from './Home';
 import AllArtists from './AllArtists';
 import AllAlbums from './AllAlbums';
+import CartView from './CartView';
 
-export { App, Nav, Home, AllArtists, AllAlbums };
+export { App, Nav, Home, AllArtists, AllAlbums, CartView };
