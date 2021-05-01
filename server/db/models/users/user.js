7286ab07c8f910cbfddaac5b6b5f8a32f34b5030
Jonathan Crider <13684007+JonathanCrider@users.noreply.github.com>
2021-05-01 16:35:05 -0400

    added auth methods to user, added auth api routes

--- server/db/models/users/user.js
+++ server/db/models/users/user.js
@@ -1,6 +1,7 @@
 const { DataTypes, Model } = require('sequelize');
 const db = require('../../db');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class User extends Model {}
 
@@ -44,6 +45,36 @@ User.addHook('beforeSave', (user) => {
   if (user._changed.has('password')) {
     user.password = bcrypt.hash(user.password, 5)
   }
-})
+});
+
+// drying out the error we're throwing for authentication
+const error = function () {
+  const err = Error('bad credentials, you suck');
+  err.status = 401;
+  return err;
+};
+
+User.authenticate = async ({ email, password }) => {
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+  if (user && bcrypt.compare(password, user.password)) {
+    return jwt.sign(user.id, process.env.JWT); // token w/ user ID
+  }
+  throw error();
+};
+
+User.byToken = async (token) => {
+  try {
+    const id = jwt.verify(token, process.env.JWT);
+    const user = await User.findByPk(id);
+    if (user) return user;
+    throw error();
+  } catch (ex) {
+    throw error();
+  }
+};
 
 module.exports = User;
