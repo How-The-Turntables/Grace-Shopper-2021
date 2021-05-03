66fa523d79bbe46710bd18edfb665716640c5ff7
Kevin Gil <93kevingil@gmail.com>
2021-05-03 11:34:26 -0400

    a little bit of a nicer navbar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 //import Calligraffitti from '../../server/public/fonts/Calligraffitti-Regular.ttf'; -- Working on figuring out changing the web font;
-import { AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box } from '@material-ui/core';
-import { ArrowBack, AssignmentInd, Apps, Home, ContactMail } from '@material-ui/icons';
+import { AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box, Button } from '@material-ui/core';
+import { ArrowBack, AssignmentInd, Apps, Home, ContactMail, VpnKey } from '@material-ui/icons';
 //import avatar from '../../server/public/img/defaultAlbum.png';
 
 class Nav extends Component {
@@ -25,10 +25,20 @@ class Nav extends Component {
       <Box component='nav'>
         <AppBar position='static' style={{ background:'#F2F1E7'
  }}>
-          <Toolbar>
-        <Typography variant='h3' style={{ color: '#42240C' }}>How The Turntables</Typography>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+          <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Typography variant='h3' style={{ color: '#42240C' }}>How The Turntables</Typography>
+            <div>
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                <Button >
+                  <Home style={{ color: '#42240C' }} />Home
+                </Button>
+              </Link>
+              <Link to="/login" style={{ textDecoration: 'none' }}>
+                <Button>
+                  <VpnKey />Login
+                </Button>
+              </Link>
+            </div>
           </Toolbar>
         </AppBar>
       </Box>
