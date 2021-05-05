8ec8082834c143aaef16684eb58572149fbe1d01
Maciej Pie <maciejpiech18@gmail.com>
2021-05-05 15:17:26 -0500

    folder change

---
+++ client/components/AdminDashboard/listItems.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleIcon from '@material-ui/icons/People';
+import Typography from '@material-ui/core/Typography';
+
+
+export const sidebarData = [
+  {
+    title: 'Orders',
+    path: '/admin',
+    icon: <DashboardIcon/>
+  },
+  {
+    title: 'Users',
+    path: '/admin/users',
+    icon: <PeopleIcon/>
+  }
+];
+
+function preventDefault(event) {
+  event.preventDefault();
+};
+
+export const NavItems = (
+  <Typography >
+   {sidebarData.map((item, index) => {
+        return (
+          <ListItem key={index}
+            onClick={preventDefault}
+            // replace
+            button>
+              <ListItemIcon>
+                {item.icon}
+              </ListItemIcon>
+              <Link to={item.path}>
+                <ListItemText primary={item.title} />
+              </Link>
+          </ListItem>
+        )
+      })}
+  </Typography>
+);
