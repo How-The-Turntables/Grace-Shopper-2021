8eb9577ee352f92b48d111f918d39aec61629ce8
Maciej Pie <maciejpiech18@gmail.com>
2021-05-06 18:07:41 -0500

    small comment

--- client/components/AllAlbums.js
+++ client/components/AllAlbums.js
@@ -107,7 +107,7 @@ class AllAlbums extends Component {
               </Typography>
               {/* <Typography variant="h5" align="center" color="textSecondary" paragraph>
               Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              Make it short and sweet, but yes not too short so folks don&apos;t simply skip over it
               entirely.
             </Typography> */}
             </Container>
