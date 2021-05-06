22dcb492eef5c23ba7ebda8586755304b1ac8c28
emilyasaro <35464891+emilyasaro@users.noreply.github.com>
2021-05-05 22:07:57 -0400

    passing to Maciej

--- client/components/SingleAlbum.js
+++ client/components/SingleAlbum.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { renderSingleAlbum } from '../redux/albums/albumActions';
 import { renderReviews } from '../redux/reviews/reviewActions';
 import { AddReview } from './index';
 
+import CircularProgress from 'material-ui/CircularProgress';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Snackbar from 'material-ui/Snackbar';
+import FlatButton from 'material-ui/FlatButton';
+// import {store} from '../../store';
+// import {PRODUCT_DETAILS_URL} from './constants';
+// import axios from 'axios';
+
 class SingleAlbum extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      review: '',
-      showHideReviewForm: false,
-    }; // this.handleAddToCart = this.handleAddToCart.bind(this)
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+          review: '',
+          showHideReviewForm: false,
+        }
+    }
 
   componentDidMount() {
     const albumId = this.props.match.params.id;
@@ -30,70 +39,81 @@ class SingleAlbum extends React.Component {
     return null;
   };
 
-  render() {
-    const { album } = this.props;
-    const { showHideReviewForm } = this.state;
-    return (
-      <div>
-        <h1>{album.title}</h1>
-
-        {!album.quantity ? (
-          <div>Sorry, this album is out of stock!</div>
-        ) : (
-          <div key={album.id}>
-            <img src={album.photoUrl} />
-            <div>
-              <div> {album.artist.name} </div>
-              <div> {album.year} </div>
-              <p> {album.description} </p>
-              <div> ${album.price} </div>
-            </div>
-            {/*add to cart button and quantity select*/}
-            <select>
-              <option></option>
-            </select>
-            <button>Add to Cart</button>
-            <div>
-              Product Reviews
-              <div>
-                {showHideReviewForm && (
-                  <AddReview history={this.props.history} id={album.id} />
-                )}
-              </div>
-              <button onClick={() => this.showComponent('showHideReviewForm')}>
-                Leave a Review
-              </button>
-              {!album.reviews ? (
-                <div>This album has not been reviewed</div>
-              ) : (
+    render() {
+        const { album } = this.props;
+        const { showHideReviewForm } = this.state;
+        if (album.error || album.fetching) {
+            return (<div><CircularProgress style={{padding: '30%'}} size={80} thickness={10}/></div>);
+        } else {
+            // this.data = this.props.productDetails.data;
+            return (
                 <div>
-                  {album.reviews.map((review) => {
-                    <div key={review.id}>
-                      {review.description}
-                      {review.stars}
-                    </div>;
-                  })}
+                    <Card>
+                        <CardHeader title={album.title} subtitle={album.artist.name}/>
+                        <CardMedia
+                            overlay={<CardTitle title={album.title} subtitle={album.artist.name} />}>
+                            <img src={album.photoUrl} alt=""/>
+                        </CardMedia>
+                        <CardTitle title={album.title} subtitle={album.artist.name}  />
+                        <CardText>{album.description}</CardText>
+                        <CardActions>
+                            <FlatButton>
+                                <Link to={'/albums/0'}>Back to Albums List</Link>
+                            </FlatButton>
+                            <FlatButton label="Add To Cart" onClick={this.addToCart.bind(this)}/>
+                        </CardActions>
+                    </Card>
+                    {/* <Snackbar
+                        open={this.state.snackBarOpen}
+                        message="Event added to your calendar"
+                        autoHideDuration={3000}
+                        onRequestClose={this.handleRequestClose.bind(this)}
+                    /> */}
                 </div>
-              )}
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    album: state.singleAlbum,
+            );
+        }
+    };
   };
-};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    loadSingle: (id) => dispatch(renderSingleAlbum(id)),
-    // add to cart
-  };
-};
+    const mapStateToProps = (state) => {
+      return {
+        album: state.singleAlbum,
+      };
+    };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleAlbum);
+    const mapDispatchToProps = (dispatch) => {
+      return {
+        loadSingle: (id) => dispatch(renderSingleAlbum(id)),
+        // add to cart
+      };
+    };
+
+    export default connect(mapStateToProps, mapDispatchToProps)(SingleAlbum);
+
+    // addToCart() {
+    //     store.dispatch({
+    //         type: 'ADD_ITEM_TO_CART',
+    //         payload: this.data
+    //     });
+    //     this.showSnackBar();
+    // }
+
+    // showSnackBar() {
+    //     this.setState({snackBarOpen: true,});
+    // }
+
+    // handleRequestClose() {
+    //     this.setState({snackBarOpen: false,});
+    // }
+
+//     static getProductDetails(nextState) {
+//         store.dispatch({
+//             type: 'FETCH_PRODUCT_DETAILS',
+//             payload: axios.get(PRODUCT_DETAILS_URL.replace('{{}}', nextState.params.sku))
+//         });
+//     }
+
+
+
+
+// export default connect(({productDetails}) => ({productDetails}))(ProductDetails);
