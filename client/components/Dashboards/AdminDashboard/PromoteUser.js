4cb522a00dfc623b3e4edd4c6ebaee0f2a3a949e
emilyasaro <35464891+emilyasaro@users.noreply.github.com>
2021-05-06 20:09:42 -0400

    still working on user >> admin

--- client/components/Dashboards/AdminDashboard/PromoteUser.js
+++ client/components/Dashboards/AdminDashboard/PromoteUser.js
@@ -20,124 +20,123 @@ const styles = (theme) => ({
 });
 
 class PromoteUser extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      // firstName: this.props.user.firstName ? this.props.user.firstName : '',
-      // lastName: this.props.user.lastName ? this.props.user.lastName : '',
-      // email: this.props.user.email ? this.props.user.email : '',
-      // password: this.props.user.password ? this.props.user.password : '',
-      admin: this.props.useradmin ? this.props.user.admin : 'Customer'
-    }
-    // this.handleSubmit = this.handleSubmit.bind(this);
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     // firstName: this.props.user.firstName ? this.props.user.firstName : '',
+  //     // lastName: this.props.user.lastName ? this.props.user.lastName : '',
+  //     // email: this.props.user.email ? this.props.user.email : '',
+  //     // password: this.props.user.password ? this.props.user.password : '',
+  //     admin: this.props.useradmin ? this.props.user.admin : 'Customer'
+  //   }
+  //   // this.handleSubmit = this.handleSubmit.bind(this);
 
-  };
-
-  // componentDidMount() {
-  //   console.log("ComponentWillMount");
-  //   const user = this.props.match.params.id
-  //   console.log("Will fetch expert with id", user);
-  //   this.props.loadUser(user);
   // };
 
-
-  componentDidUpdate = (prevProps) => {
-    // const user = this.props.match.params.id
-    if(!prevProps.user.id && user.id){
-      this.setState({
-      //   firstName: this.props.user.firstName,
-      //   lastName: this.props.user.lastName,
-      //   email: this.props.user.email,
-      //   password: this.props.user.password,
-        admin: this.props.user.admin
-      })
-    }
+  componentDidMount() {
+    console.log("ComponentWillMount");
+    const user = this.props.match.params.id
+    console.log("Will fetch expert with id", user);
+    this.props.loadUser(user);
   };
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      // const user = JSON.parse(window.localStorage.getItem('UserCart'));
-      // await this.props.updateUser(user.id, this.state );
-      await this.props.promoteUser(this.props.match.params.id, this.state );
 
-    }
-    catch (err) {
-      this.setState({ error: err.response.data.error });
-    }
-  };
+  // componentDidUpdate = (prevProps) => {
+  //   // const user = this.props.match.params.id
+  //   if(!prevProps.user.id && user.id){
+  //     this.setState({
+  //     //   firstName: this.props.user.firstName,
+  //     //   lastName: this.props.user.lastName,
+  //     //   email: this.props.user.email,
+  //     //   password: this.props.user.password,
+  //       admin: this.props.user.admin
+  //     })
+  //   }
+  // };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+  // handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     // const user = JSON.parse(window.localStorage.getItem('UserCart'));
+  //     // await this.props.updateUser(user.id, this.state );
+  //     await this.props.promoteUser(this.props.match.params.id, this.state );
+
+  //   }
+  //   catch (err) {
+  //     this.setState({ error: err.response.data.error });
+  //   }
+  // };
+
+  // handleChange = (event) => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   });
+  // };
 
   render() {
     const { classes } = this.props;
     const { admin } = this.state
 
     return (
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-        onSubmit={ this.handleSubmit }>
-        <div>
-          {/* <TextField
-            disabled
-            id="filled-disabled"
-            label="Disabled"
-            value={firstName}
-            name="firstName"
-            onChange={ this.handleChange }
-            />
-            <TextField
-            disabled
-            id="filled-disabled"
-            label="Disabled"
-            value={lastName}
-            name="lastName"
-            onChange={ this.handleChange }
-            />
-            <TextField
-            disabled
-            id="filled-disabled"
-            label="Disabled"
-            value={email}
-            name="email"
-            onChange={ this.handleChange }
-            /> */}
-          <TextField
+      <React.Fragment>
+        <Title>Recent Orders</Title>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Ship To</TableCell>
+              <TableCell>Payment Method</TableCell>
+              <TableCell align="right">Sale Amount</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.length ? orders.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.createdAt}</TableCell>
+                <TableCell>{row.status}</TableCell>
+                <TableCell>{row.user.firstName} {row.user.lastName}</TableCell>
+                <TableCell>{row.paymentMethod}</TableCell>
+                <TableCell align="right">{row.total}</TableCell>
+              </TableRow>
+            )) : 'no orders yet'}
+          </TableBody>
+        </Table>
+        <div className={classes.seeMore}>
+          <Link color="primary" href="#" onClick={preventDefault}>
+            See more orders
+          </Link>
+        </div>
+      </React.Fragment>
+    );
+  };
+};
+
+{/* <TextField
           required
           id="filled-disabled"
           label="Admin Status"
           value={admin}
           name="admin"
           onChange={ this.handleChange }
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            size="small"
-            className={classes.button}
-            startIcon={<SaveIcon />}>Save
-         </Button>
-        </div>
-      </form>
-    );
-  };
-};
+          /> */}
+        //   <Button
+        //     type="submit"
+        //     variant="contained"
+        //     color="primary"
+        //     size="small"
+        //     className={classes.button}
+        //     startIcon={<SaveIcon />}>Save
+        //  </Button>
 
 const mapStateToProps = (state, ownProps) => {
   console.log('STATE ',state )
-  const user = state.users.find(campus => campus.id === otherProps.match.params.id*1) || {};
+  // const user = state.users.find(user => user.id === ownProps.match.params.id*1) || {};
+  //   return {
+  //     user
+  //   }
     return {
-      campus
-    }
-    return {
-      promoteUser: state.promoteUser,
+      // promoteUser: state.promoteUser,
       selectedUser: state.selectedUser
     }
 };
@@ -145,8 +144,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, { history }) => {
 	return {
-    promoteUser: ( id, user ) => dispatch( renderPromoteUser( id, user, history )),
-    loadUser: ( id ) => dispatch( renderSelectedUser( id ))
+    // promoteUser: ( id, user ) => dispatch( renderPromoteUser( id, user, history )),
+    loadUser: ( id ) => dispatch( renderSelectedUser( id, history ))
 	}
 };
 
