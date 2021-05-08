b0bc1e49c839603063ea54306dd310f251ba3b18
emilyasaro <35464891+emilyasaro@users.noreply.github.com>
2021-05-08 09:52:09 -0400

    reviews coming together

--- client/components/ReviewViews/AddReview.js
+++ client/components/ReviewViews/AddReview.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { renderAddReview } from '../../redux/reviews/reviewActions';
+import { withStyles } from '@material-ui/core/styles';
 
 //STYLES
-import { Button } from '@material-ui/core';
+import { Grid, Select, MenuItem, TextField, Button } from '@material-ui/core';
+
+
+const styles = (theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+});
 
 class AddReview extends React.Component {
   constructor(props) {
@@ -31,6 +43,8 @@ class AddReview extends React.Component {
     });
   };
   render() {
+    const { classes } = this.props;
+
     return (
       <div >
         <form
@@ -50,7 +64,31 @@ class AddReview extends React.Component {
             required >
           </textarea>
           <br/>
-
+          <Grid item xs={12}>
+            <TextField
+              type="text"
+              name="comment"
+              value={this.state.comment}
+              onChange={this.handleChange}
+              placeholder="How was the album?"
+              required
+            />
+          </Grid>
+          <Select
+            // value={stars}
+            onChange={this.handleChange}
+            displayEmpty
+            className={classes.selectEmpty}
+            inputProps={{ 'aria-label': 'Without label' }}>
+            <MenuItem value="">
+              <em>Rating</em>
+            </MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+        </Select>
           {/* <label>Star Rating</label>
           <br/>
           <input
@@ -82,4 +120,4 @@ const mapDispatchToProps = (dispatch, { history }) => {
   }
 };
 
-export default connect( null, mapDispatchToProps )( AddReview );
+export default withStyles(styles, { withTheme: true })(connect( null, mapDispatchToProps )( AddReview ));
